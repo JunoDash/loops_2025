e692d8db75dce8f6e3cf069676237eca06c4a690
JunoDash <jperezreyes@cps.edu>
2025-12-11 15:11:53 +0000

    did work

--- 4_problem_set_1.py
+++ 4_problem_set_1.py
@@ -22,16 +22,16 @@
 # Ask the user for a number **n**, then calculate the **factorial** of that number.
 
 # *(Example: factorial of 5 is 120)
-def factorial(n):
+# def factorial(n):
 
-    factorial = 1
+#     factorial = 1
 
-    for i in  range(n):
-        factorial *= i + 1
+#     for i in  range(n):
+#         factorial *= i + 1
     
-    return factorial
+#     return factorial
 
-print(factorial(5))
+# print(factorial(5))
 # ### **Problem 4: Count Vowels**
 
 # Ask the user for a string. Count and print how many **vowels (a, e, i, o, u)** are in the string.
@@ -40,22 +40,26 @@ print(factorial(5))
 # ### **Problem 5: Print Even Numbers**
 
 # Ask the user for a number **n**, then print all **even numbers** from 2 up to n.
-n = int(input("Enter a number: "))
-for number in range(2, n + 1, 2):
-    print(number)
+# n = int(input("Enter a number: "))
+# for number in range(2, n + 1, 2):
+#     print(number)
 
-list_even_numbers = list(range(1, 45))
-for numbers in list_even_numbers:
-    if number % 2 == 0:
-        print("even number", number)
-    else:
-        print("odd number, skipping", number)
+# list_even_numbers = list(range(1, 45))
+# for numbers in list_even_numbers:
+#     if number % 2 == 0:
+#         print("even number", number)
+#     else:
+#         print("odd number, skipping", number)
 # ### **Problem 6: Reverse a String**
 
 # Ask the user for a string, then print the string **backwards**.
-
-
-
+# name = input("Enter a string: ")
+# reversed_name = ""
+# for char in name:
+#     reversed_name = char + reversed_name
+#     print(reversed_name)
+# print(reversed_name)
+# print(reversed_name[::-1])
 # ### **Problem 7: Multiplication Table**
 
 # Ask the user for a number **n**, then print the **multiplication table** for n from
@@ -73,9 +77,24 @@ for numbers in list_even_numbers:
 # ### **Problem 9: Fibonacci Sequence**
 
 # Ask the user for a number **n**, then print the first **n numbers** of the Fibonacci sequence.
+# recurstion means a function calls itself
+# def car_price(n):
+#     if n <= 0:
+#         return 0
+#     elif n == 1:
+#         return 1
+#     else:
+#         return car_price(n - 1) + car_price(n - 2)
 
+def finioncci(n):
 
+    if n == 1 or n in [1, 2]:
+        return 1
 
+    return finioncci(n - 1) + finioncci(n - 2)
+
+for i in range(1, 10):
+    print(finioncci(i))
 # ### **Problem 10: Pattern Printing**
 
 # Ask the user for a number **n**, then print a pattern of stars where the first row has 1 star, the second has 2, and so on until row n.
